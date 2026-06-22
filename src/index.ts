fda7a8967d0b8fd0f1d912deaf6e10b33533830e
Samuel Titus Oluwatimileyin <oluwatimileyinsamuel2018@gmail.com>
2026-06-23 00:26:41 +0100

    add Vercel frontend URL to CORS whitelist

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
+    'https://nigerian-banking-frontend.vercel.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
